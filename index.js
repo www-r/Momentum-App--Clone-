2d5612f1c51a3e4d4744325748e17a950539a45a
www-r <117294002+www-r@users.noreply.github.com>
2022-11-28 23:44:14 +0900

    if savedusername exists or not

--- index.js
+++ index.js
@@ -2,16 +2,33 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector("#greeting");
-const HIDDEN_CLASSNAME = "hidden"; //관습:string만 포함된 변수는 대문자로 쓴다
+
+ //string만 포함된 변수는 대문자로 쓴다->오타로 인한 에러 방자하기위해.
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event){
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem("username", username)
-    greeting.innerText = `Hello ${username}`;//=== greeting.innerText = "Hello " + username ;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(username);
 }
-loginForm.addEventListener("submit", onLoginSubmit );
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
+}
 
 
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if(savedUserName===null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit );
+
+} else {
+    greeting.innerText = `Hello ${savedUserName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
+}
