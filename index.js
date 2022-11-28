4aaed367eef706e66f07b014bbfa070bb1c06c72
www-r <117294002+www-r@users.noreply.github.com>
2022-11-28 23:17:29 +0900

    save username in localStorage
    local storage: allows us to save things on the browser
    
    setItem , getItem , removeItem

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector("#greeting");
-const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_CLASSNAME = "hidden"; //관습:string만 포함된 변수는 대문자로 쓴다
 
 function onLoginSubmit(event){
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-   // greeting.innerText = "Hello " + username ;
-    greeting.innerText = `Hello ${username}`;
+    localStorage.setItem("username", username)
+    greeting.innerText = `Hello ${username}`;//=== greeting.innerText = "Hello " + username ;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 loginForm.addEventListener("submit", onLoginSubmit );
