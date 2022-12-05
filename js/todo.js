f90c8fb84b686a04cfc80f9f011062a5e31ceff9
www-r <117294002+www-r@users.noreply.github.com>
2022-12-05 10:49:46 +0900

    Update todo.js
    -save todos in local storage
    -problem: 중복입력이 안됨. array 형태로 저장하고 싶음

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,26 @@ const todoInput = todoForm.querySelector("input");
 // const todoInput = document.querySelector(#todo-form input);
 const todoList = document.getElementById("todo-list");
 
+const todos = [];
+
+function saveTodos(){
+    localStorage.setItem("todos",todos) //local storage can only save strings
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintTodo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteTodo );
+    li.appendChild(span);
+    li.appendChild(button);
     todoList.appendChild(li);
 }
 
@@ -17,6 +32,8 @@ function handleTodoSubmit(event){
     const newTodo = todoInput.value; 
     //copying the current value of the input tnto a new variable
     todoInput.value = "";
+    todos.push(newTodo);
     paintTodo(newTodo);
+    saveTodos();
 } 
 todoForm.addEventListener("submit", handleTodoSubmit);
