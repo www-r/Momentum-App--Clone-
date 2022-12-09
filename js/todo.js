e1571c005a953ac2a30a301e2e2bb638c04ce02b
www-r <117294002+www-r@users.noreply.github.com>
2022-12-09 18:28:45 +0900

    weather API

--- js/todo.js
+++ js/todo.js
@@ -1,39 +1,56 @@
-const todoForm = document.getElementById("todo-form");
-const todoInput = todoForm.querySelector("input");
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
 // const todoInput = document.querySelector(#todo-form input);
-const todoList = document.getElementById("todo-list");
+const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
+let toDos = []; //to update the old toDos
 
-const todos = [];
-
-function saveTodos(){
-    localStorage.setItem("todos",JSON.stringify(todos)) 
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos)) 
  //local storage can only save strings=> save into a form of array (not real array)
 }//JSON stringify => take JS object or an array and make into a strings.
-function deleteTodo(event){
+
+function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo)=> toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
-function paintTodo(newTodo){
+function paintToDo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "X";
-    button.addEventListener("click", deleteTodo );
+    button.addEventListener("click", deleteToDo );
     li.appendChild(span);
     li.appendChild(button);
-    todoList.appendChild(li);
+    toDoList.appendChild(li);
 }
 
 //forma은 submit를 가지고, submit하면 default 로 새로고침이 된다.
-function handleTodoSubmit(event){
+function handleToDoSubmit(event){
     event.preventDefault();
-    const newTodo = todoInput.value; 
+    const newTodo = toDoInput.value; 
     //copying the current value of the input tnto a new variable
-    todoInput.value = "";
-    todos.push(newTodo);
-    paintTodo(newTodo);
-    saveTodos();
+    toDoInput.value = "";
+    const newTodoObj = {
+        text:newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
 } 
-todoForm.addEventListener("submit", handleTodoSubmit);
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    //JSON.parse() => make string into an array
+    toDos = parsedToDos; //update todos
+    parsedToDos.forEach(paintToDo);
+}
